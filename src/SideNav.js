0530e70a01928d394d78ac26ea6ec69f34dcb6a7
kanupriya1801 <kanupriya18701@gmail.com>
2023-02-04 22:56:04 +0530

    Message

---
+++ src/SideNav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+export default function SideNav(props) {
+    return (
+        <ul className='side-nav' >
+            <li className='inbox' onClick={() => { props.change("INBOX") }}>
+                <div>INBOX</div>
+            </li>
+            <li className='today' onClick={() => { props.change("TODAY") }}>
+                <div>TODAY</div>
+            </li>
+            <li className='next' onClick={() => { props.change("NEXT") }}>
+                <div>NEXT 7 DAYS</div>
+            </li>
+        </ul>
+    )
+}
